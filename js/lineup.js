d9ab8956cd12992611fc9aa02d3b19bd490c0785
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-09-30 00:34:42 -0300

    dados de banco

--- js/lineup.js
+++ js/lineup.js
@@ -8,8 +8,7 @@ $("document").ready(function () {
             for (let index = 0; index < data.length; index++) {
                 dados = data[index];
                 console.log(dados);
-                $('#lista > tbody:last-child').append('<tr><td><a href="https://www.marinetraffic.com/en/ais/details/ships/' + dados.imo + '"><i data-feather="anchor"></i></a></td><td>' + dados.navio + '</td><td>' + dados.status + '</td><td>' + dados.eta + '</td><td>' + dados.etb + '</td><td>' + dados.ets + '</td><td>' + dados.wtime + '</td><td>' + dados.cargo + '</td><td>' + dados.agency + '</td><td>' + dados.qtd + '</td></tr>');
-                feather.replace()
+                $('#lista > tbody:last-child').append('<tr><td><a href="https://www.marinetraffic.com/en/ais/details/ships/' + dados.imo + '"><i class="fas fa-ship"></i></a></td><td>' + dados.navio + '</td><td>' + dados.status + '</td><td>' + dados.eta + '</td><td>' + dados.etb + '</td><td>' + dados.ets + '</td><td>' + dados.wtime + '</td><td>' + dados.cargo + '</td><td>' + dados.agency + '</td><td>' + dados.qtd + '</td></tr>');
             }
         }
     });
