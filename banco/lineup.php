4c61f6192c1c4d942d18fb8d3b8344d7a411ff67
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-10-02 00:39:50 -0300

    paginas e procedures para atracações e navios

--- banco/lineup.php
+++ banco/lineup.php
@@ -1,23 +1,53 @@
 <?php
 include_once('../../conexao_ln.php');
-function retorna($con){
-    $queryln = "call lineup()";
-    $prepara = $con->prepare($queryln);
-    $prepara->execute();
-    $contar = $prepara->RowCount();
-
-    if($contar>0){
-        while($linha = $prepara->fetch(PDO::FETCH_ASSOC)){
-            $linhas[] = $linha;
-        }
-        $prepara->closeCursor();
-        return json_encode($linhas);
+function retorna($con, $tipo)
+{
+    switch ($tipo) {
+        case '1':
+            
+            $queryln = "call lineup()";
+            $prepara = $con->prepare($queryln);
+            $prepara->execute();
+            $contar = $prepara->RowCount();
+        
+            if ($contar>0) {
+                while ($linha = $prepara->fetch(PDO::FETCH_ASSOC)) {
+                    $linhas[] = $linha;
+                }
+                $prepara->closeCursor();
+                return json_encode($linhas);
+            }
+        break;
+        case '2':
+            
+            $queryln = "call navios()";
+            $prepara = $con->prepare($queryln);
+            $prepara->execute();
+            $contar = $prepara->RowCount();
+        
+            if ($contar>0) {
+                while ($linha = $prepara->fetch(PDO::FETCH_ASSOC)) {
+                    $linhas[] = $linha;
+                }
+                $prepara->closeCursor();
+                return json_encode($linhas);
+            }
+        break;
+        case '3':
+            
+            $queryln = "call atracacoes()";
+            $prepara = $con->prepare($queryln);
+            $prepara->execute();
+            $contar = $prepara->RowCount();
+        
+            if ($contar>0) {
+                while ($linha = $prepara->fetch(PDO::FETCH_ASSOC)) {
+                    $linhas[] = $linha;
+                }
+                $prepara->closeCursor();
+                return json_encode($linhas);
+            }
+        break;
     }
 }
-echo retorna($con);
-
-
-
-
-
-?>
+echo retorna($con, $_GET['tipo']);
--- js/lineup.js
+++ js/lineup.js
@@ -2,7 +2,7 @@
 $("#lineup").ready(function () {
     $.ajax({
         type: "POST",
-        url: "banco/lineup.php?l=1",
+        url: "banco/lineup.php?tipo=1",
         dataType: "json",
         success: function (data) {
             for (let index = 0; index < data.length; index++) {
@@ -13,17 +13,30 @@ $("#lineup").ready(function () {
         }
     });
 });
-
 $("#navios").ready(function () {
     $.ajax({
         type: "POST",
-        url: "banco/lineup.php?l=2",
+        url: "banco/lineup.php?tipo=2",
         dataType: "json",
         success: function (data) {
             for (let index = 0; index < data.length; index++) {
                 dados = data[index];
                 console.log(dados);
-                $('#navios > tbody:last-child').append('<tr><td><a href="https://www.marinetraffic.com/en/ais/details/ships/' + dados.imo + '"><i class="fas fa-ship"></i></a></td><td>' + dados.navio + '</td><td>' + dados.status + '</td><td>' + dados.eta + '</td><td>' + dados.etb + '</td><td>' + dados.ets + '</td><td>' + dados.wtime + '</td><td>' + dados.cargo + '</td><td>' + dados.agency + '</td><td>' + dados.qtd + '</td></tr>');
+                $('#navios > tbody:last-child').append('<tr><td><a href="https://www.marinetraffic.com/en/ais/details/ships/' + dados.imo + '"><i class="fas fa-ship"></i></a></td><td>' + dados.navio + '</td><td>' + dados.imo + '</td><td>' + dados.atracacoes + '</td></tr>');
+            }
+        }
+    });
+});
+$("#atracacoes").ready(function () {
+    $.ajax({
+        type: "POST",
+        url: "banco/lineup.php?tipo=3",
+        dataType: "json",
+        success: function (data) {
+            for (let index = 0; index < data.length; index++) {
+                dados = data[index];
+                console.log(dados);
+                $('#atracacoes > tbody:last-child').append('<tr><td><a href="https://www.marinetraffic.com/en/ais/details/ships/' + dados.imo + '"><i class="fas fa-ship"></i></a></td><td>' + dados.navio + '</td><td>' + dados.status + '</td><td>' + dados.eta + '</td><td>' + dados.etb + '</td><td>' + dados.ets + '</td><td>' + dados.wtime + '</td><td>' + dados.cargo + '</td><td>' + dados.agency + '</td><td>' + dados.qtd + '</td></tr>');
             }
         }
     });
