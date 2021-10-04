34e4c237e111e491d88574dc24f615255e861631
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-10-04 00:11:53 -0300

    Situações

--- js/atracacoes.js
+++ js/atracacoes.js
@@ -7,7 +7,7 @@ $("#atracacoes").ready(function () {
             for (let index = 0; index < data.length; index++) {
                 dados = data[index];
                 console.log(dados);
-                $('#atracacoes > tbody:last-child').append('<tr><td><a href="https://www.marinetraffic.com/en/ais/details/ships/' + dados.imo + '"><i class="fas fa-ship"></i></a></td><td>' + dados.navio + '</td><td>' + dados.status + '</td><td>' + dados.eta + '</td><td>' + dados.etb + '</td><td>' + dados.ets + '</td><td>' + dados.wtime + '</td><td>' + dados.cargo + '</td><td>' + dados.agency + '</td><td>' + dados.qtd + '</td></tr>');
+                $('#atracacoes > tbody:last-child').append('<tr><td><a href="https://www.marinetraffic.com/en/ais/details/ships/' + dados.imo + '"><i class="fas fa-ship"></i></a></td><td>' + dados.navio + '</td><td><span class="badge rounded-pill '+dados.bootstrap_class+'">'+dados.situacao+'</span></td><td>' + dados.eta + '</td><td>' + dados.etb + '</td><td>' + dados.ets + '</td><td>' + dados.wtime + '</td><td>' + dados.cargo + '</td><td>' + dados.agency + '</td><td>' + dados.qtd + '</td></tr>');
             }
         }
     });
